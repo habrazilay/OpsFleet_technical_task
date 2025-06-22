169932d2f221220f2193aa7c7a8286c1b92540ee
Daniel Schmidt <dsch87@gmail.com>
2025-06-22 17:44:27 +0300

    fix: correct backend path for make targets

--- environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "${var.tf_state_bucket}"
-    key            = "eks-karpenter/${var.project}/terraform.tfstate"
-    region         = "${var.aws_region}"
-    dynamodb_table = "${var.tf_state_dynamodb_table}"
-    encrypt        = true
-  }
-}
